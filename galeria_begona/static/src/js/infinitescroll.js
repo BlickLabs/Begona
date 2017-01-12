50e2dabc3753884c6086bf9d3a22b151a4378406
Alan Sanchez <sanchezpineda03@gmail.com>
2017-01-12 15:45:49 -0600

    Test infinite scroll on gallery.

---
+++ galeria_begona/static/src/js/infinitescroll.js
@@ -0,0 +1,45 @@
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: ".gallery-wrapper #loader", triggerHook: "onEnter"})
+	.addTo(controller)
+	.on("enter", function (e) {
+		if (!$(".gallery-wrapper #loader").hasClass("active")) {
+			$(".gallery-wrapper #loader").addClass("active");
+			if (console){
+				console.log("loading new items");
+			}
+			// simulate ajax call to add content using the function below
+			setTimeout(getPhotos, 1000, 9);
+		}
+	});
+
+function getPhotos() {
+  var data = { csrf: $('[name="csrfmiddlewaretoken"').val() };
+  $.ajax({
+    url: 'https://jsonplaceholder.typicode.com/posts/',
+    method: 'GET',
+    data: $.param(data),
+    headers: {
+      'X-CSRFToken': data.csrf,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  })
+    .done(function (data) {
+      var currentElements = $('#carousel-bounding-box .carousel-inner').children().length;
+      data.slice(0, 9).forEach(function (item) {
+        $('<div class="col-sm-4 spacer"></div>')
+          .appendTo('.gallery-elements')
+          .append('<div class="gallery-img" data-toggle="modal" data-target="#galleryOne"><a href="#myCarousel" data-slide-to="' + currentElements + '"><img src="http://dev.com:8000/static/img/galeria/large/gallery1.jpg" class="img-responsive" alt="Gallery" /></a></div>');
+
+    		$('<div class="item" data-slide-number="' + currentElements + '"></div>')
+    			.appendTo("#carousel-bounding-box .carousel-inner")
+          .append('<img src="http://dev.com:8000/static/img/galeria/large/gallery1.jpg">');
+
+        currentElements += 1;
+      });
+      scene.update();
+    	$(".gallery-wrapper #loader").removeClass("active");
+    });
+}
