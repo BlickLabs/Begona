4021fb9331e0552c6f875f1215187ac751bff747
Alan Sanchez <sanchezpineda03@gmail.com>
2017-01-12 19:33:41 -0600

    Fixed carousel bug.

--- galeria_begona/static/src/js/products.js
+++ galeria_begona/static/src/js/products.js
@@ -4,15 +4,13 @@
     $('a[data-toggle="portfilter"]').click(function (e) {
       var tag = $(this).attr('data-target'),
         elements = null,
-        links = $('[data-tag*="' + tag + '"] a');
+        links = tag === 'all' ? $('[data-tag] a') : $('[data-tag*="' + tag + '"] a');
       allElements.removeClass('active').removeAttr('style');
       elements = allElements.filter('[data-categories*="' + tag + '"]');
 
       $('#ProductOne .carousel-inner').children().remove();
       $('#ProductOne .carousel-inner').append(elements);
       $('#ProductOne .carousel-inner .item').first().addClass('active');
-      console.log(elements);
-      console.log(links);
       links.toArray().forEach(function (item) {
         var $item = $(item),
           index = elements.index($('[data-slide-number="' + $item.attr('data-original-slide-to') + '"]'));
