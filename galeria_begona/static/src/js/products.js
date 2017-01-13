c11d5fbce2f581e38089400ad894f1df2655b78d
Alan Sanchez <sanchezpineda03@gmail.com>
2017-01-12 19:27:34 -0600

    Improved products carousel.

---
+++ galeria_begona/static/src/js/products.js
@@ -0,0 +1,27 @@
+(function () {
+  function products() {
+    var allElements = $('#ProductOne .carousel-inner .item');
+    $('a[data-toggle="portfilter"]').click(function (e) {
+      var tag = $(this).attr('data-target'),
+        elements = null,
+        links = $('[data-tag*="' + tag + '"] a');
+      allElements.removeClass('active').removeAttr('style');
+      elements = allElements.filter('[data-categories*="' + tag + '"]');
+
+      $('#ProductOne .carousel-inner').children().remove();
+      $('#ProductOne .carousel-inner').append(elements);
+      $('#ProductOne .carousel-inner .item').first().addClass('active');
+      console.log(elements);
+      console.log(links);
+      links.toArray().forEach(function (item) {
+        var $item = $(item),
+          index = elements.index($('[data-slide-number="' + $item.attr('data-original-slide-to') + '"]'));
+          $item.attr('data-slide-to', index);
+      });
+
+    });
+  }
+  if ($('.product').length) {
+    products();
+  }
+})();
