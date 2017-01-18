23c1005f9830f53798b30a26c80b994f3deb592f
Alan Sanchez <sanchezpineda03@gmail.com>
2017-01-17 19:40:59 -0600

    Django default parameters.

--- galeria_begona/apps/landing/views.py
+++ galeria_begona/apps/landing/views.py
@@ -24,27 +24,27 @@ class ContactTemplateView(TemplateView):
 class ProductsTemplateView(TemplateView):
     template_name = 'landing/products.html'
 
-def get_product_context_data(self, classView, **kwargs):
+def get_product_context_data(self, classView, defaultProduct, defaultPrice, **kwargs):
     ctx = super(classView, self).get_context_data(**kwargs)
-    ctx['product'] = self.request.GET.get('producto')
-    ctx['color'] = self.request.GET.get('color')
-    ctx['price'] = int(self.request.GET.get('precio'))
+    ctx['product'] = self.request.GET.get('producto', defaultProduct)
+    ctx['color'] = self.request.GET.get('color', 'Negro')
+    ctx['price'] = int(self.request.GET.get('precio', defaultPrice))
     return ctx
 
 class BootTemplateView(TemplateView):
     template_name = 'landing/pedido-bota.html'
 
     def get_context_data(self, **kwargs):
-        return get_product_context_data(self, BootTemplateView, **kwargs)
+        return get_product_context_data(self, BootTemplateView, 'Bota clásica', 6542, **kwargs)
 
 class BootyTemplateView(TemplateView):
     template_name = 'landing/pedido-botin.html'
 
     def get_context_data(self, **kwargs):
-        return get_product_context_data(self, BootyTemplateView, **kwargs)
+        return get_product_context_data(self, BootyTemplateView, 'Botín clásico', 2400, **kwargs)
 
 class ChapsTemplateView(TemplateView):
     template_name = 'landing/pedido-chaparrera.html'
 
     def get_context_data(self, **kwargs):
-        return get_product_context_data(self, ChapsTemplateView, **kwargs)
+        return get_product_context_data(self, ChapsTemplateView, 'Chaparrera 1', 6042, **kwargs)
